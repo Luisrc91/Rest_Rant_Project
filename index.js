f3872cf0bd7ce28a35143ee74b1939c8c78ffeed
Luis <luisrc91@live.com>
2023-02-18 13:54:33 -0500

    adding 404 page view

--- index.js
+++ index.js
@@ -15,9 +15,13 @@ app.get('/', (req, res) => {
   })
   
 
+// app.get('*', (req, res) => {
+//     res.status(404).send('<h1>404 Page</h1>')
+// })
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
-})
+    res.render('error404')
+  })
+  
 
 app.listen(process.env.PORT, () => {
     console.log("I AM ALIVE!");
