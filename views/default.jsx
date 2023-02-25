450021db1bfd283bbc01c74a7e92c460911a6780
Luis <luisrc91@live.com>
2023-02-25 11:19:46 -0500

    navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -11,6 +11,20 @@ function Def (html) {
 
             </head>
             <body>
+            <nav>
+              <ul>
+                <li>
+                  <a href="/">Home</a>
+                </li>
+                <li>
+                  <a href="/places">Places</a>
+                </li>
+                <li>
+                  <a href="/places/new">Add Place</a>
+                </li>
+              </ul>
+            </nav>
+
                 {html.children}
             </body>
         </html>
