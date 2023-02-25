80faf101a597578a706366d3074d38f377cd04b2
Luis <luisrc91@live.com>
2023-02-25 10:16:53 -0500

    fixed error pic

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 // Controllers & Routes
 app.use('/places', require('./controllers/places'))
@@ -19,7 +20,6 @@ app.get('/', (req, res) => {
 app.get('*', (req, res) => {
     res.render('error404')
 })
-app.use(express.urlencoded({ extended: true }))
 
 // Listen for Connections
 app.listen(process.env.PORT)
